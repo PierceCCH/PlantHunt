70d127737e13ad0b1ae59aa519a25236b3e43278
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2023-04-22 10:19:15 -0700

    login page

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Home from "./src/Home";
+import Home from "./src/Views/Home";
 import { ModelProvider } from "./src/ModelContext";
+import Login from "./src/Views/Login";
 
 export default function App() {
   return (
     <ModelProvider>
+      {/* <Login /> */}
       <Home />
     </ModelProvider>
   );
