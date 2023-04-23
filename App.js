8e090b8ec583a6f750c36e7789fef0a1e3313855
ZnPR0N00BnZ <limzhineng@hotmail.com>
2023-04-22 17:09:09 -0700

    Navbar now has highlighted buttons on touch Improving scroll left and right so game header and navbar remains on screen
    To do:
    Swiping is in sync with navbar (eg swiping to left updates highlighted buttons too)

--- App.js
+++ App.js
@@ -6,42 +6,51 @@ import CameraView from "./Views/Camera";
 import HomeView from "./Views/Home";
 import { useRef, useState, useEffect } from "react";
 import BaseTemplate from "./Views/BaseTemplate";
+//import Swiper from "react-native-swiper";
 
 const { width, height } = Dimensions.get("window");
 
 export default function App() {
-  const scrollViewRef = useRef();
-  useEffect(() => {
-    // Scroll to the rightmost edge of the ScrollView
-    scrollViewRef.current.scrollToEnd({ animated: false });
-  }, []);
   return (
-    <ScrollView
-      ref={scrollViewRef}
-      snapToInterval={width}
-      decelerationRate="fast"
-      onContentSizeChange={(contentWidth, contentHeight) => {
-        // Set the initial position of the ScrollView to the rightmost edge
-        scrollViewRef.current.scrollTo({
-          x: contentWidth - scrollViewRef.current.clientWidth,
-          animated: false,
-        });
+    <View
+      style={{
+        width: width,
+        height: height,
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      horizontal
     >
       <BaseTemplate>
         <CameraView />
-      </BaseTemplate>
-      <BaseTemplate>
         <HomeView />
       </BaseTemplate>
-    </ScrollView>
+    </View>
   );
 }
 
-// <NavigationContainer>
-//       <Stack.Navigator initialRouteName="home">
-//         <Stack.Screen name="home" component={HomeView} />
-//         <Stack.Screen name="camera" component={CameraView} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
+// //outside return
+// const scrollViewRef = useRef();
+// useEffect(() => {
+//   // Scroll to the rightmost edge of the ScrollView
+//   scrollViewRef.current.scrollToEnd({ animated: false });
+// }, []);
+
+// //in return
+// <ScrollView
+//   ref={scrollViewRef}
+//   snapToInterval={width}
+//   decelerationRate="fast"
+//   onContentSizeChange={(contentWidth, contentHeight) => {
+//     // Set the initial position of the ScrollView to the rightmost edge
+//     scrollViewRef.current.scrollTo({
+//       x: contentWidth - scrollViewRef.current.clientWidth,
+//       animated: false,
+//     });
+//   }}
+//   horizontal
+// >
+//   <BaseTemplate>
+//     <CameraView />
+//     <HomeView />
+//   </BaseTemplate>
+// </ScrollView>;
