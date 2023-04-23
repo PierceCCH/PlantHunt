a5927806701f33f47485a1be466b74695c2e7974
ZnPR0N00BnZ <limzhineng@hotmail.com>
2023-04-23 02:40:44 -0700

    Views template for Info, Leaderboard, My Collection, Grids of Plants

--- Views/Adventure.js
+++ Views/Adventure.js
@@ -1,33 +1,70 @@
 import React from "react";
-import { Text, View, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 import tailwind from "tailwind-rn";
 import LogEntry from "../src/LogEntry";
+import { useState } from "react";
 
 const { width, height } = Dimensions.get("window");
+const containers = {
+  width: width,
+  //height: height,
+  justifyContent: "center",
+  alignItems: "center",
+};
+const styles = StyleSheet.create({
+  containers: {
+    width: width,
+    //height: height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  info: { ...containers, flex: 1 },
+  leaderboard: {
+    ...containers,
+    flex: 2,
+    backgroundColor: "green",
+    borderRadius: 20,
+    marginLeft: "10%",
+    marginRight: "10%",
+  },
+  myCollectionHeader: { ...containers, flex: 0.5 },
+  myCollection: { ...containers, flex: 4, backgroundColor: "green" },
+});
 const AdventureView = () => {
   return (
-    <View
-      style={{
-        width: width,
-        height: height,
-        // justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#ECECEC",
-        paddingTop: "15%",
-      }}
-    >
-      <View>
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: "24",
-            marginBottom: "5%",
-          }}
-        >
-          THIS IS THE ADVENTURE PAGE
-        </Text>
+    <SafeAreaView>
+      <View
+        style={{
+          width: width,
+          height: height,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#ECECEC",
+          flexDirection: "column",
+        }}
+      >
+        <View style={styles.info}>
+          <Text>Info</Text>
+        </View>
+        <View style={styles.leaderboard}>
+          <Text>Leaderboard</Text>
+        </View>
+        <View style={styles.myCollectionHeader}>
+          <Text>My Collection</Text>
+        </View>
+        <View style={styles.myCollection}>
+          <Text>Insert Grid of Plants here</Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
